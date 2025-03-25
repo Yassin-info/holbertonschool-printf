70a6a74b3ddcc49a58c4ebeb13e83c0b9b3a8e27
User <user@MacBook-Air-de-User.local>
2025-03-25 11:28:45 +0100

    update the code

--- _printf.c
+++ _printf.c
@@ -1,9 +1,22 @@
 #include "main.h"
 /**
 * 
- */
+*/
 int_printf(const char *format, ...)
 {
-    int i;
-    int count = 0;
     
+    int count = 0;
+    const char *c;
+    va_list args;
+
+    if (format == NULL)
+    return (-1);
+
+    va_start(args, format);
+
+    if (c = '%')
+    {
+        c++;
+        if (c = '\0')
+        {
+            return (-1);
