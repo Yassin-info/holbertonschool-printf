6cbb0ab4e08f9529015684f2cce229a0288ae7c2
User <user@air-de-user.home>
2025-03-26 12:08:15 +0100

    fixing error and test the code

---
+++ printf2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_int - imprime un entier sur sortie standard
+* @args: liste arguments contenat entier a imprimer
+* Return: nombre de chiffe a imprimer
+*/
+int print_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n = -n;
+	}
+	count += print_number(n);
+
+	return (count);
+}
+/**
+ * print_number - imprime nombre en utilisant recursion
+ * @num: nombre a imprimer
+ * Return: nombre de chiffre a imprimer
+ */
+
+int print_number(unsigned int num)
+{
+	int count = 0;
+
+	if (num / 10)
+		count += print_number(num / 10);
+	
+    return (count + _putchar(num % 10 + '0'));
+	
+    count += _putchar(num % 10 + '0');
+
+	return (count);
+}
